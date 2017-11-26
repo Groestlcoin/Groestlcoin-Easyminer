0f4690174dabada3c1fd823aeb2fbfb322181491
yokom <yokom@ADAM-PC>
2017-11-26 16:09:36 +0000

    Add global exception messagebox for any unhandled exceptions to prevent crashing.

--- GroestlCoin EasyMiner 2017/App.xaml.cs
+++ GroestlCoin EasyMiner 2017/App.xaml.cs
@@ -6,12 +6,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace GroestlCoin_EasyMiner_2017
-{
+namespace GroestlCoin_EasyMiner_2017 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App : Application {
+        public App() : base() {
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        void OnDispatcherUnhandledException(object sender,
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
+            string errorMessage = string.Format("An unhandled exception occurred: {0}", e.Exception.Message);
+            MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
